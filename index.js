dfdc15b1da7b351cf692cda37c5528d8a2d92774
Rahul Patil <rahulrohidaspatil@gmail.com>
2023-05-17 02:36:13 +0530

    @solana/web3.js added

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
+import bs58 from 'bs58';
 import Aptos from './aptos.js';
 import Solana from './solana.js';
 
 const aptos = new Aptos();
 const solana = new Solana();
 
-aptos.test();
-solana.test();
+function getAllPropertiesOfClass(classParam) {
+    return [...Object.getOwnPropertyNames(classParam), ...Object.getOwnPropertyNames(classParam.prototype)];
+}
+
+function getDecodedPrivateKey(privateKey) {
+    return Uint8Array.from(bs58.decode(privateKey));
+}
+
+async function solTransfer() {
+    // const fromKeypair = solana.generateKeypair();
+    const fromKeypair = solana.getKeypairFromSecretKey(getDecodedPrivateKey('5Gwv1vC7z375WLtpZCQb4V3kuKR37WwhFnAyBbyDJ3riKxZWoqs5uVC2bMcxUcNbFdFBUdBoSpf544U3szmRydvc'));
+    const toKeypair = solana.generateKeypair();
+    
+    // const airdropSignature = await solana.requestAirdrop(
+    //     fromKeypair.publicKey,
+    //     10000000000,
+    // );
+    // const confirmedTransaction = await solana.confirmTransaction(airdropSignature);
+    
+    const transaction = solana.getNewTransaction();
+
+    transaction.add(solana.getTransferInstruction(fromKeypair.publicKey, toKeypair.publicKey, 1000000000));
+    const response = await solana.sendTransaction(solana.connection, transaction, fromKeypair);
+    console.log(response);
+}
+
+solTransfer();
--- solana.js
+++ solana.js
@@ -1,5 +1,37 @@
+import { Connection, Keypair, SystemProgram, Transaction, clusterApiUrl, sendAndConfirmTransaction, PublicKey } from '@solana/web3.js';
+
 export default class Solana {
-    test() {
-        console.log('solana test');
+    connection = new Connection(clusterApiUrl('devnet')); // A connection to a fullnode JSON RPC endpoint
+
+    getKeypairFromSecretKey(secretKey) {
+        return Keypair.fromSecretKey(secretKey); // Create a keypair from a raw secret key byte array
+    }
+
+    generateKeypair() {
+        return Keypair.generate(); // Generate a new random keypair
+    }
+
+    requestAirdrop(to, lamports) {
+        return this.connection.requestAirdrop(to, lamports); // Request an allocation of lamports to the specified address
+    }
+
+    confirmTransaction(signature) {
+        return this.connection.confirmTransaction({ signature });
+    }
+
+    getNewTransaction() {
+        return new Transaction(); // Transaction class
+    }
+
+    getTransferInstruction(fromPubkey, toPubkey, lamports) {
+        return SystemProgram.transfer({ // Generate a transaction instruction that transfers lamports from one account to another
+            fromPubkey, // Account that will transfer lamports
+            toPubkey, // Account that will receive transferred lamports
+            lamports, // Amount of lamports to transfer
+        });
+    }
+
+    sendTransaction(connection, transaction, keypair) {
+        return sendAndConfirmTransaction(connection, transaction, [keypair]); // Sign, send and confirm a transaction
     }
 }
